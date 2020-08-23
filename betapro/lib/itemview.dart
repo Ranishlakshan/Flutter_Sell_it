60e1deac1eca405d3b8cae6a3977b3b740d3af02
Ranishka Lakshan <ranishkalakshangsc@gmail.com>
2020-08-23 13:01:02 +0530

    add full details - error one listview builder

--- betapro/lib/itemview.dart
+++ betapro/lib/itemview.dart
@@ -1,6 +1,8 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 
+
+
 class ItemView extends StatefulWidget {
   final String docID123;
 
@@ -22,12 +24,7 @@ class _ItemViewState extends State<ItemView> {
       appBar: AppBar(
         title: Text('Item Data'),
       ),
-      body: ListView(
-        children: <Widget>[
-          Text(widget.docID123),
-          getItems(),
-        ],
-      ),
+      body: getItems()
     );
   }
 }
@@ -39,10 +36,71 @@ Widget getItems(){
       stream: Firestore.instance.collection('ads').document('1597917013710').snapshots(),
       builder: (context, snapshot){
         if (!snapshot.hasData) {
+          //snapshot.data.toString();
           return new Text("Loading");
         }
+        DocumentSnapshot dd = snapshot.data; 
+
+        //
+
+        //
         var userDocument = snapshot.data;
-        return new Text(snapshot.data['brand']);
+        //String myname = dd.data.toString();
+        int len = dd.data.length;
+        //String myname = dd.data.toString();
+        //List<String> list_name = new List(len);
+        //List<String> list = myname.split(", ");
+        //String nochar = myname.replaceAll(new RegExp(r"[^\s\w]"),'');
+        //var ddd = myname.split(', ').map((String text) => Text(text));
+        //return new Text(snapshot.data['brand']);
+        //String sss = ddd.
+        //
+        String jsonString = dd.data.toString();
+        String start = "[";
+        String end = "]";
+        final startIndex = jsonString.indexOf(start);
+        final endIndex = jsonString.indexOf(end, startIndex + start.length);
+        String next = jsonString.substring(startIndex + start.length, endIndex);
+        String imagelinkRemoved = jsonString.replaceAll(next, "");
+        String urlremoved = imagelinkRemoved.replaceAll("urls: [], ", "").replaceAll("{", "").replaceAll("}", "");
+
+        List<String> spec_list = urlremoved.split(", ");
+        int speclistlen = spec_list.length;
+        
+        //
+        return ListView.builder(
+                itemCount: speclistlen,
+                itemBuilder: (context,index){
+                  return Card(
+                    //child: Text(spec_list[index])
+                    child: Column(
+                      children: <Widget>[
+                        Text(spec_list[index]),
+                        
+                      ],
+                    ),
+                    );
+                },
+              
+        );
+
+        //int i=0;
+        //if(i<speclistlen){
+        //  return Text (spec_list[i]);
+        //} 
+        //return Text(speclistlen.toString());
+        //for(int i;i<5;i++){
+        //  return Text(spec_list[i]);
+        //}
+        //List<String> items = ;
+        //return new Text(list[9]);
+        //return new Text(spec_list[9]);
+        //return ListView(
+        //  
+        //  children: <Widget>[
+        //    
+        //  ],
+        //);
       }
     );
 }
